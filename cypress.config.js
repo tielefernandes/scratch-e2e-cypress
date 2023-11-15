3b5ab8fbcfbf357ad309fe569c30bf6e586c06db
tielefernandes <tielefernandes@gmail.com>
2023-11-15 16:34:17 -0300

    :test_tube: test: [iFrame] testando iframe

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+	chromeWebSecurity: false,
 	e2e: {
-
 		baseUrl: 'https://notes-serverless-app.com',
-		setupNodeEvents(on, config) {
+		setupNodeEvents() {
 			// implement node event listeners here
 		},
 	},
